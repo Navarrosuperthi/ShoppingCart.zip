c79d1cf97f2bee9ac60711c6807dcf40f5ec1d13
FabioBritto <fabio.tritono@gmail.com>
2024-06-05 14:10:13 -0300

    Commit project ShoppingCart.zip

---
+++ src/ShoppingCart.java
@@ -0,0 +1,18 @@
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName = "Steve";
+        int spaceIdx = 0;
+        
+        // Obtenha o índice do caractere de espaço (" ") em custName.
+        custName.indexOf(" ");
+
+        // Use o método substring, analise o primeiro nome e imprima-o.
+        custName.substring(0, spaceIdx);
+        
+        System.out.println(firstName);
+
+    }
+
+    
+}
